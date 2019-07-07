a233e8b80e1ee5588454b88cce624a52d0f08d30
jarabt <jarabt@seznam.cz>
2019-07-07 21:25:08 +0200

    dnes koncim 2019-07-07

---
+++ Jp08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = new int[10];
+            for (int i = 0; i < cisla.Length; i++)
+                cisla[i] = 10 - i;
+            foreach (int n in cisla)
+                Console.WriteLine(n);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { 1, 2, 3, 3, 3, 5 };
+            Console.WriteLine(cisla.Length);
+            cisla = cisla.Distinct().ToArray();
+            Console.WriteLine(cisla.Length);
+            foreach (int n in cisla)
+                Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
