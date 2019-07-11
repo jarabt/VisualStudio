08392b56b04169850ffcfc8d10149598ab7089a9
jarabt <jarabt@seznam.cz>
2019-07-11 22:16:16 +0200

    Lekce 9 pred ascii hodnota

---
+++ l09p01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace l09p01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Ahoj ITnetwork";
+            Console.WriteLine(s);
+            Console.WriteLine(s[2]);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ l09p02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace l09p02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string we want to analyze
+            string s = "Programátor se zasekne ve sprše, protože instrukce na šampónu byly: " +
+                "Namydlit, omýt, opakovat.";
+            Console.WriteLine(s);
+            s = s.ToLower();
+
+            // inicializace počítadel
+            int pocetSamohlasek = 0;
+            int pocetSouhlasek = 0;
+
+            // definice typů znaků
+            string samohlasky = "aeiouyáéěíóúůý";
+            string souhlasky = "bcčďdfghjklmnqprřsštťvwxzž";
+
+            // hlavní cyklus
+            foreach (char c in s)
+            {
+                if (samohlasky.Contains(c))
+                    pocetSamohlasek++;
+                else if (souhlasky.Contains(c))
+                    pocetSouhlasek++;
+            }
+
+            Console.WriteLine("Samohlásek: {0}", pocetSamohlasek);
+            Console.WriteLine("Souhlásek: {0}", pocetSouhlasek);
+            Console.WriteLine("Nepísemných znaků: {0}", s.Length - (pocetSouhlasek + pocetSamohlasek));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
