fa8f2096d3057b28f2e44e33489993f77da1a80e
jarabt <jarabt@seznam.cz>
2019-07-11 20:40:21 +0200

    k lekci 8

--- Spp08/Program.cs
+++ Spp08/Program.cs
@@ -9,6 +9,27 @@ namespace Spp08
     {
         static void Main(string[] args)
         {
+            string[] zelenina = { "zelí", "okurka", "rajče", "paprika", "ředkev", "mrkev", "brokolice" };
+            string[] ovoce = { "jablko", "hruška", "pomeranč", "jahoda", "banán", "kiwi", "malina" };
+            string dale = "ano";
+            int count = 0;
+
+            while (dale == "ano")
+            {
+                Console.Write("Zadej název libovolného ovoce nebo zeleniny: ");
+                string zadani = Console.ReadLine().Trim().ToLower();
+                if (zelenina.Contains(zadani))
+                    Console.WriteLine("Zadal jsi zeleninu");
+                else if (ovoce.Contains(zadani))
+                    Console.WriteLine("Zadal jsi ovoce");
+                else
+                    Console.WriteLine("Tvoje slovo nemám v seznamu");
+                count++;
+                Console.WriteLine("Přeješ si zadat další slovo? (ano/ne)");
+                dale = Console.ReadLine().Trim().ToLower();
+            }
+            Console.WriteLine("Zadal jsi {0} slov", count);
+            Console.ReadKey();
         }
     }
 }
